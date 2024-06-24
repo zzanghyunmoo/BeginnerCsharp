ecdd25cdce6ecbfeb1292c3a810f096d7a2afff4
gurumee92 <gurumee92@woowahan.com>
2024-06-24 23:01:17 +0900

    learn: 5-33.변수 명 짓기 팁, 키워드드

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,15 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            int y = 20;
+            // 코딩 규칙 1. 변수 명은 의미 있게 지어야 한다!
+            int appleCnt = 10;
+            int orangeCnt = 20;
 
-            Console.WriteLine($"{x} + {y} = {x + y}");
-            Console.WriteLine($"{x} - {y} = {x - y}");
-            Console.WriteLine($"{x} * {y} = {x * y}");
-            Console.WriteLine($"{x} / {y} = {(double)x / y}");
+            Console.WriteLine($"{appleCnt} + {orangeCnt} = {appleCnt + orangeCnt}");
+            Console.WriteLine($"{appleCnt} - {orangeCnt} = {appleCnt - orangeCnt}");
+            Console.WriteLine($"{appleCnt} * {orangeCnt} = {appleCnt * orangeCnt}");
+            Console.WriteLine($"{appleCnt} / {orangeCnt} = {(double)appleCnt / orangeCnt}");
 
-            // constant는 대문자로 구성한다!
+            // 코딩 규칙 2. constant는 대문자로 구성한다!
             const float PI = 3.14f;
             Console.WriteLine(PI);
         }
