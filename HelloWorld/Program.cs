f6d04f13f54ea5533b0cfb67a0991b44867a17fc
gurumee92 <gurumee92@woowahan.com>
2024-06-24 22:29:21 +0900

    learn: 4-29. 여러 값 출력하기

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,18 +6,19 @@ namespace HelloWorld
 {
     class Program
     {
-        // Main 함수란? f(x) = Main(args)
-        // 프로그램의 시작점(entry point), c#은 반드시 Main 함수부터 시작이 된다.
-        // static이란? 전역 함수. oop랑은 관련 없는 친구
-        // args? 매개변수, 외부로부터 Main 함수로 전달하는 데이터
-        // void? 반환 값이 없는 함수.
-        // int? 반환 값이 정수. 0 반환 시, 정상 종료를 의미함.
-        static int Main(string[] args)
+        // statement? 
+        // 1. 한줄짜리 코드. 끝에 ; 있음.
+        // 2. 여러 줄의 코드로 이루어진 블록. { } 로 감싸져 있음. ex) if
+        // comment?
+        // 코드와 관련된 내용을 메모할 때 사용. 컴파일 할 때 무시되는 문장. // /* */ 로 달 수 있음.
+        static void Main(string[] args)
         {
-            // Console.WriteLine = 입력 받은 문자열을 화면에 출력한 후 개행해주는 함수.
-            // 함수란? 특정 기능을 수행하는 코드의 집합이다.
-            Console.WriteLine("Hello World!");
-            return 0;
+            // (...) 에서 ...은 상수! 
+            Console.Write(30);        // int
+            Console.WriteLine(30.1);  // double
+            Console.WriteLine(30.2f); // float
+            Console.Write(true);      // bool
+            Console.Write('p');       // char
         }
     }
 }
