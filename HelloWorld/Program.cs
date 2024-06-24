1888f1da5a3a00081b33edab86f14124eee45027
gurumee92 <gurumee92@woowahan.com>
2024-06-24 23:06:06 +0900

    learn: 5-33.변수 명 짓기 팁, 키워드 #2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,9 +15,16 @@ namespace HelloWorld
             Console.WriteLine($"{appleCnt} * {orangeCnt} = {appleCnt * orangeCnt}");
             Console.WriteLine($"{appleCnt} / {orangeCnt} = {(double)appleCnt / orangeCnt}");
 
+            appleCnt += 20;
+            orangeCnt -= 10;
+            Console.WriteLine($"apple: {appleCnt} orange: {orangeCnt}");
+
             // 코딩 규칙 2. constant는 대문자로 구성한다!
             const float PI = 3.14f;
             Console.WriteLine(PI);
+
+            // 코딩 규칙 3. 변수는 소문자 두 단어 이상이면 단어 첫 부분을 대문자 appleCnt
+            // 코딩 규칙 4. 상수는 모두 대문자. 두 단어 이상이면 _로 구문 APPLE_CNT
         }
     }
 }
