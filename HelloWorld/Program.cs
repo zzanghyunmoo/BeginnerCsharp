a90626c9c3ab39638ba2afc6abbc079f5f8c2b27
gurumee92 <gurumee92@woowahan.com>
2024-06-24 22:57:06 +0900

    learn: 5-32. 변수란? #2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
             int y = 20;
 
             Console.WriteLine($"{x} + {y} = {x + y}");
+            Console.WriteLine($"{x} - {y} = {x - y}");
+            Console.WriteLine($"{x} * {y} = {x * y}");
+            Console.WriteLine($"{x} / {y} = {(double)x / y}");
 
             // constant는 대문자로 구성한다!
             const float PI = 3.14f;
