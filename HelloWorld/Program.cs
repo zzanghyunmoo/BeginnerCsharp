c5ed1223521dd485533e618685cb15f00628e2ec
gurumee92 <gurumee92@woowahan.com>
2024-06-27 23:03:37 +0900

    learn: 5-36. 각 자료형마다 비트수가 다른 이유

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,11 +10,20 @@ namespace HelloWorld
             byte b = 0; // 8bit
             short s = 0; // 16bit
             int i = 0; // 32bit
-            long l = 0l; // 64bit
+            long l = 0L; // 64bit
             float f = 0f; // 32bit
             double d = 0d; // 64bit
             char c = 'a'; // 16bit (unicode
             bool bo = false; // 1bit  true or false
+
+            Console.WriteLine($"byte: {b}");
+            Console.WriteLine($"short: {s}");
+            Console.WriteLine($"int: {i}");
+            Console.WriteLine($"long: {l}");
+            Console.WriteLine($"float: {f}");
+            Console.WriteLine($"double: {d}");
+            Console.WriteLine($"char: {c}");
+            Console.WriteLine($"bool: {bo}");
         }
     }
 }
