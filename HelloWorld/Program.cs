91abfdce8114421daa37ae83b4c745161d5484fa
gurumee92 <gurumee92@woowahan.com>
2024-07-01 23:50:41 +0900

    learn: 6-42. implicit conversion #3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,6 +9,12 @@ namespace HelloWorld
             int num1 = 1234567890;
             float num2 = num1;
             Console.WriteLine(num1 - (int)num2);
+
+            double num3 = 3.7;
+            int num4 = 10;
+            Console.WriteLine(num3 - num4);
+            Console.WriteLine((int)num3 - num4);
+            Console.WriteLine((int)(num3 - num4));
         }
     }
 }
