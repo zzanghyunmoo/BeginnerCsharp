9cb4470dd723e3e584558c97053c914278eddcc9
gurumee92 <gurumee92@woowahan.com>
2024-07-01 23:43:36 +0900

    learn: 6-42. implicit conversion

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,24 +4,11 @@ namespace HelloWorld
 {
     class Program
     {
-        // Primitives Types. 기본 자료형. 하드웨어가 이해하는 자료형 
         static void Main(string[] args)
         {
-            byte maxByte = byte.MaxValue;
-            byte minByte = byte.MinValue;
-
-            int maxInt = int.MaxValue;
-            int minInt = int.MinValue;
-
-            long maxLong = long.MaxValue;
-            long minLong = long.MinValue;
-
-            Console.WriteLine($"max byte: {maxByte}");
-            Console.WriteLine($"min byte: {minByte}");
-            Console.WriteLine($"max int: {maxInt}");
-            Console.WriteLine($"min int: {minInt}");
-            Console.WriteLine($"max long: {maxLong}");
-            Console.WriteLine($"min long: {minLong}");
+            int num = 100000;
+            long num2 = num;
+            Console.WriteLine(num2);
         }
     }
 }
